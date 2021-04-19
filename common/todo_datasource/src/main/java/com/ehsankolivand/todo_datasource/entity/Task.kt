2fe430103705af9b5ffffcf3035645add7e44532
ehsankolivand <ehsankolivandeh@gmail.com>
2021-04-18 23:49:35 -0700

    Add Task Entity and TaskDao for room database

---
+++ common/todo_datasource/src/main/java/com/ehsankolivand/todo_datasource/entity/Task.kt
@@ -0,0 +1,47 @@
+package com.ehsankolivand.todo_datasource.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+import com.ehsankolivand.constants.Constants
+
+
+@Entity
+class Task(
+    @field:ColumnInfo(name = "Name") val name: String?,
+    /** Work duration, in minutes.  */
+    @field:ColumnInfo(name = "WorkDuration") val workDuration: Int,
+    /** Break duration, in minutes.  */
+    @field:ColumnInfo(name = "BreakDuration") val breakDuration: Int,
+    @field:ColumnInfo(name = "LongBreaks") val isLongBreaks: Boolean,
+    /** Long break duration, in minutes.  */
+    @field:ColumnInfo(name = "LongBreakDuration") val longBreakDuration: Int,
+    @field:ColumnInfo(name = "SessionsBeforeLongBreak") val sessionsBeforeLongBreak: Int,
+    @field:ColumnInfo(
+        name = "DND"
+    ) val isDND: Boolean,
+    @field:ColumnInfo(name = "KeepDNDOnBreaks") val isKeepDNDOnBreaks: Boolean,
+    @field:ColumnInfo(name = "WiFi") val isWiFi: Boolean,
+    @field:ColumnInfo(name = "showInStatistics") val isShowInStatistics: Boolean
+) {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "ID")
+    var id = 0L
+
+    @Ignore
+    constructor(name: String?) : this(
+        name,
+        Constants.DEFAULT_WORK_TIME,
+        Constants.DEFAULT_BREAK_TIME,
+        true,
+        Constants.DEFAULT_LONG_BREAK_TIME,
+        Constants.DEFAULT_SESSIONS_BEFORE_LONG_BREAK,
+        false,
+        false,
+        false,
+        true
+    ) {
+    }
+
+}
